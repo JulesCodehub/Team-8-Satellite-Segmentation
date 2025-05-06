f5a5fcd855680ff556b51cead0ddfa1d1a09de48
Jules <180057196+JulesCodehub@users.noreply.github.com>
2025-05-06 09:24:26 -0500

    Create test_job.sh

---
+++ SLURM Scripts/test_job.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=unetImageSegBaseline
+#SBATCH --output=logs/unetIS_output_%j.txt
+#SBATCH --error=logs/unetIS_error_%j.txt
+#SBATCH --time=12:00:00
+#SBATCH --partition=kimq
+#SBATCH --gres=gpu:1
+#SBATCH --mem=8G
+
+echo "Activating My custom environment"
+source ~/myEnvs/tf_env/bin/activate
+
+cd ~/SwinTransformer
+
+echo "Running T8 Test"
+python tools/train.py configs/pspnet_unet_s5-d16_64x64_40k_drive.py --options model.pretrained=swin_transformer.py [model.backbone.use_checkpoint=True]
+
+echo "Deactivating TensorFlow-2.6.2 environment"
+deactivate
+
+echo "Done."
